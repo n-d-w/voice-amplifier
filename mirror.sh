2d6de530d116b81687ae3495f736fd31d0f273ed
Dev Who Ops <next.devops.world@gmail.com>
2026-07-07 23:42:50 +0200

    Voice aplifier scritps

---
+++ mirror.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PREFIX_URL="https://hlasyagresora.eu"
+DATA="osoby kategorie"
+user_agent="User-agent: Voice Amplifier"
+
+for dt in $DATA; do
+	curl -X GET --silent -H "$user_agent" $PREFIX_URL/api/$dt.php \
+		| jq --sort-keys . > hlasyagresora.eu/$dt.json
+done
+
+FOTOS=$( jq -r '.[].foto' hlasyagresora.eu/osoby.json )
+for foto in $FOTOS; do
+	if [[ "$foto" =~ ^[a-zA-Z0-9._-]+$ ]]; then
+		echo "Image: '$foto'"
+		curl -X GET --silent -H "$user_agent" $PREFIX_URL/uploads/$foto > hlasyagresora.eu/uploads/$foto
+	else
+		echo "Invalid image: 'foto'"
+	fi
+done
+	
